8a82a9defc07b2a7d6927db9657a4a7a811d8ae4
mahasahyoun <maha.z.sahyoun@gmail.com>
2020-04-16 21:16:43 +0300

    Update lab-8

---
+++ lab-8/net-salary.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	float basic_salary, tax, sales, bonus, net_salary;
+
+	cout << "Please enter the basic salary for the employee: ";
+	cin >> basic_salary;
+
+	cout << "Please enter the sales for the employee: ";
+	cin >> sales;
+
+	tax = basic_salary * 0.10;
+
+	if (sales < 5)
+	{
+		cout << "Bonus: " << 0 << endl;
+		bonus = 0;
+	}
+
+	else 
+	{
+		if (sales >= 5 && sales <= 50)
+		{
+			cout << "Bonus: " << 5;
+			bonus = 5;
+		}
+
+		else
+		{
+			if (sales >= 51 && sales <= 100)
+			{
+				cout << "Bonus: " << 10;
+				bonus = 10;
+			}
+
+			else 
+			{
+				if (sales >= 101 && sales <= 200)
+				{
+					cout << "Bonus: " << 20;
+					bonus = 20;
+				}
+
+				else
+				{
+					if (sales >= 201 && sales <= 500)
+					{
+						cout << "Bounus:" << 50;
+						bonus = 50;
+					}
+
+					else
+					{
+						cout << "Bonus: " << 70;
+						bonus = 70;
+					}
+				}
+			}
+		}
+	}
+				
+	net_salary = basic_salary + bonus - tax;
+		
+	cout << endl << "Net Salary: " << net_salary << endl;
+			
+	return 0;
+}
